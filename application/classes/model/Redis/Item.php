570217bbdfd553cd4acaa671f5d85738858cab34
magnax <magnax@gmail.com>
2012-08-29 18:24:20 +0200

    add dummy files

--- application/classes/model/Redis/Item.php
+++ application/classes/model/Redis/Item.php
@@ -1,20 +1,7 @@
 <?php
 
-class Model_Redis_Item extends Model_Item {
-    
-    public function fetchAll($filter = null) {
-        if ($filter) {
-            $ids = $this->source->smembers("global:items:$filter");
-        } else {
-            $ids = $this->source->smembers("global:items");
-        }
-        if (count($ids)) {
-            return array(1=>'bone_knife');
-        } else {
-            return null;
-        }
-    }
-    
-}
-
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
 ?>
