d9fffd49acfe34bc1a644b9188570733943e837d
magnax <magnax@gmail.com>
2012-08-29 10:14:23 +0200

    add dummy file

--- application/classes/controller/Base/Admin.php
+++ application/classes/controller/Base/Admin.php
@@ -1,43 +1,7 @@
 <?php
 
-/**
- * kontroler dla administratorów
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
  */
-class Controller_Base_Admin extends Controller_Template {
-
-    public $template = 'templates/admin';
-
-    protected $redis = null;
-
-    public function before() {
-        parent::before();
-        $page = ($this->request->directory ? $this->request->directory.'/' : '')
-            . $this->request->controller.'/'.$this->request->action;
-
-        if (Kohana::find_file('views', $page)) {
-            $this->template->content = View::factory($page);
-        } else {
-            $this->template->content = 'brakuje pliku views/'.$page.'.php :)';
-        }
-
-        $this->view = $this->template->content;
-
-        /**
-         * inicjalizacja i połączenie z Redisem
-         */
-        $this->redis = new Predis_Client(array(
-            'host'     => '127.0.0.1',
-            'port'     => 6379,
-            'database' => 0,
-            'alias' => 'mn'
-        ));
-
-        try {
-            $this->redis->select('mn');
-        } catch (Predis_CommunicationException $e) {
-            $this->redirectError('Server Redis nie uruchomiony');
-        }
-    }
-}
-
 ?>
