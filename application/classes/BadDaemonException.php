38ed7b7f878ea9a5836fa10e20ea07327303b88c
magnax <magnax@gmail.com>
2012-08-29 11:22:51 +0200

    add dummy files

--- application/classes/BadDaemonException.php
+++ application/classes/BadDaemonException.php
@@ -1,5 +1,7 @@
-<?php defined('SYSPATH') or die('No direct access');
+<?php
 
-class BadDaemonException extends Exception {
-    
-}
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
