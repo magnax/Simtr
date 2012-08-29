838a8f4ba8a408ec92dec52947dc161d980bc9f0
magnax <magnax@gmail.com>
2012-08-29 18:08:51 +0200

    add dummy files

--- application/classes/model/LNames/Redis.php
+++ application/classes/model/LNames/Redis.php
@@ -1,32 +1,7 @@
 <?php
 
-class Model_LNames_Redis extends Model_LNames {
-
-    public function getName($id_location) {
-        
-        if ($this->character_id) {
-            $name = $this->source->get("characters:{$this->character_id}:lnames:$id_location");
-        } else {
-            $name = null;
-        }
-        
-        if (!$name) {
-            $name = $this->dict->getString('Nienazwane miejsce');
-        }
-        
-        return $name;
-    }
-
-    function setName($id_location, $new_name) {
-
-        if (!strlen($new_name)) {
-            $this->source->del("characters:{$this->character_id}:lnames:$id_location");
-        } else {
-            $this->source->set("characters:{$this->character_id}:lnames:$id_location", $new_name);
-        }
-
-    }
-
-}
-
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
 ?>
