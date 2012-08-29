bf5f3d4d1099098308bf94bac80d89507aedac83
magnax <magnax@gmail.com>
2012-08-29 17:29:37 +0200

    updated mail module, add email sending after register

---
+++ application/config/general_example.php
@@ -0,0 +1,11 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+return array(
+    'paths' => array(
+        'time_daemon_path' => '/home/user/path/d.py',
+    ),
+    'server_ip' => 'http://xx.xx.xx.xx:8000',
+    'site_url' => 'http://some.domain.name/',
+);
+
+?>
