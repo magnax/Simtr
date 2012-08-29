faecd6ebd874b43e4b1b479d27dbe34dfd579ce7
magnax <magnax@gmail.com>
2012-08-29 10:16:54 +0200

    add base controllers folder

---
+++ application/classes/controller/base/admin.php
@@ -0,0 +1,12 @@
+<?php  defined('SYSPATH') or die('No direct script access.');
+
+/**
+ * kontroler dla administratorów
+ */
+class Controller_Base_Admin extends Controller_Base_User {
+
+    public $template = 'templates/admin';
+
+}
+
+?>
