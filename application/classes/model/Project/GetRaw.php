e5df0577e9ed8bbc5307eadd2095f956696db015
magnax <magnax@gmail.com>
2012-08-29 18:22:22 +0200

    add dummy files

--- application/classes/model/Project/GetRaw.php
+++ application/classes/model/Project/GetRaw.php
@@ -1,20 +1,7 @@
 <?php
 
-class Model_Project_GetRaw extends Model_Project {
-
-    protected $amount;
-    protected $resource_id;
-
-    public function toArray() {
-
-        $tmp_arr = parent::toArray();
-        $tmp_arr['amount'] = $this->amount;
-        $tmp_arr['resource_id'] = $this->resource_id;
-
-        return $tmp_arr;
-        
-    }
-
-}
-
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
 ?>
