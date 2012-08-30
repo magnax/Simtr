1c0d839ac024c5614bb0e271057b2e8422eadac6
magnax <magnax@gmail.com>
2012-08-30 12:50:37 +0200

    remove deprecated methods

--- application/classes/controller/user/menu.php
+++ application/classes/controller/user/menu.php
@@ -52,69 +52,6 @@ class Controller_User_Menu extends Controller_Base_User {
     }
 
     /**
-     * formularz tworzenia nowej postaci
-     */
-    public function action_newform() {
-
-    }
-    
-    /**
-     * sprawdzenie i utworzenie nowej postaci
-     * "spawning" w losowej lokacji
-     * przypisanie początkowych współczynników
-     */
-    public function action_new() {
-
-        //generate event & message
-        $event = Model_EventSender::getInstance(
-            Model_Event::getInstance(
-                Model_Event::SPAWN, $this->game->getRawTime(), $this->redis
-            )
-        );
-        
-        if (!Arr::get($_POST, 'name') || !Arr::get($_POST, 'sex')) {
-            $this->redirectError('Musi być podane imię oraz płeć', '/u/menu/newform');
-        }
-
-        //określenie lokacji początkowej
-            //tak będzie praget widłowo a na razie wszyscy w jednej lokacji ;)
-            //$id_location = $this->redis->srandmember('global:locations');
-        $id_location = 1;
-        
-        $character_data = array(
-            'name' => Arr::get($_POST, 'name'),
-            'sex' => Arr::get($_POST, 'sex'),
-            'location_id' => $id_location,
-            'spawn_date' => $this->game->getRawTime(),
-            'user_id' => $this->user->getID(),
-        );
-        
-        //create (and save) new character
-        $character = Model_Character::getInstance($this->redis)
-            ->createNew($character_data);
-
-        $location = Model_Location::getInstance($this->redis)
-            ->findOneByID($id_location, $character->getId());
-
-        //append chracter to location
-        $location->appendCharacter($character->getId());
-        
-        //append character to user
-        $this->user->appendCharacter($character->getId());
-
-        
-
-        //recipients to lista obiektów klasy Character
-        $event->addRecipients($location->getAllVisibleCharacters('loc'));
-        $event->setSender($character->getId());
-        $event->setLocationType($location->getType());
-        $event->send();
-
-        $this->request->redirect('u/menu');
-
-    }
-
-    /**
      * ustawia bieżącą postać - wszystkie akcje będą dotyczyły właśnie
      * tej postaci
      *
