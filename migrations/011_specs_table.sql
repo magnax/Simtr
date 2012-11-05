f3d0c0526a9b4501bac9e54c0f12157d8997b6c1
magnax <magnax@gmail.com>
2012-11-05 10:42:58 +0100

    specs table migration fix

--- migrations/011_specs_table.sql
+++ migrations/011_specs_table.sql
@@ -1,15 +1,16 @@
-CREATE TABLE  `specs` (
-`id` INT NOT NULL AUTO_INCREMENT ,
-`itemtype_id` INT NOT NULL ,
-`time` INT NOT NULL ,
-PRIMARY KEY (  `id` )
-) ENGINE = INNODB;
+CREATE TABLE IF NOT EXISTS `specs` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `itemtype_id` int(11) NOT NULL,
+  `buildmenu_id` int(11) NOT NULL,
+  `time` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `buildmenu_id` (`buildmenu_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
 
-INSERT INTO  `specs` (
-`id` ,
-`itemtype_id` ,
-`time`
-)
-VALUES (
-NULL ,  '1',  '3600'
-);
+--
+-- Zrzut danych tabeli `specs`
+--
+
+INSERT INTO `specs` (`id`, `itemtype_id`, `buildmenu_id`, `time`) VALUES
+(1, 1, 2, 3600),
+(2, 3, 1, 86400);
