791070d007d747e2a43b3b173f2abbdf49ab655e
magnax <magnax@gmail.com>
2012-11-11 19:29:53 +0100

    fix corpse/character rename

--- application/views/chname/index.php
+++ application/views/chname/index.php
@@ -1,10 +1,11 @@
+<?php if ($character->life): ?>
 Mów do tej postaci: <br />
 <form action="<?php echo url::site('user/char/talkto'); ?>" method="POST">
     <input type="hidden" name="character_id" value="<?php echo $character_id; ?>">
     Co chcesz powiedzieć: <input type="text" name="text" value=""><br />
     <input type="submit" value="Mów">
 </form>
-
+<?php endif; ?>
 Zmiana nazwy postaci<br />
 Obecna nazwa: <?php echo $name; ?>
 <?= form::open(); ?>
@@ -12,8 +13,10 @@ Obecna nazwa: <?php echo $name; ?>
     Nowa nazwa: <input type="text" name="name" value="<?php echo $name; ?>"><br />
     <input type="submit" value="Zmień">
 </form>
+<?php if ($character->life): ?>
 <p><?php echo html::anchor('user/people/hit/'.$character_id, '[Atakuj]'); ?></p>
 <div id="char_stats">
     Obecny stan:<br />
     Zdrowie: <?php echo Utils::conditionBar($character->life); ?>
 </div>
+<?php endif; ?>
