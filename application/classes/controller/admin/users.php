7ea9cfb4769f8c7c90a88112ea8a854608d4fd6e
mn <mn@MAGNUS>
2013-03-04 13:35:06 +0100

    class names fix

--- application/classes/controller/admin/users.php
+++
@@ -1,9 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.');
-
-class Controller_Admin_Users extends Controller_Base_Admin {
-    
-    public function action_index() {
-        
-    }
-    
-}
