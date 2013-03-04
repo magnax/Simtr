381954723c824c338555a8e7772d2637b4bac18b
mn <mn@MAGNUS>
2013-03-04 15:29:57 +0100

    fix class names

--- application/config/auth.php
+++ application/config/auth.php
@@ -2,7 +2,7 @@
  
 return array(
  
-    'driver'       => 'orm',
+    'driver'       => 'ORM',
     'hash_method'  => 'sha256',
     'hash_key'     => 'Never gonna give you up',
     'lifetime'     => 1209600,
