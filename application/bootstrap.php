d8567ee0db073108e89cf5f7ac898422d72eb75f
mn <mn@MAGNUS>
2013-03-04 14:32:09 +0100

    bootstrap fix for KO3.3

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -3,17 +3,14 @@
 // -- Environment setup --------------------------------------------------------
 
 // Load the core Kohana class
-require SYSPATH.'classes/kohana/core'.EXT;
+require SYSPATH . 'classes/Kohana/Core' . EXT;
 
-if (is_file(APPPATH.'classes/kohana'.EXT))
-{
-	// Application extends the core
-	require APPPATH.'classes/kohana'.EXT;
-}
-else
-{
-	// Load empty core extension
-	require SYSPATH.'classes/kohana'.EXT;
+if (is_file(APPPATH . 'classes/Kohana' . EXT)) {
+    // Application extends the core
+    require APPPATH . 'classes/Kohana' . EXT;
+} else {
+    // Load empty core extension
+    require SYSPATH . 'classes/Kohana' . EXT;
 }
 
 /**
@@ -113,76 +110,5 @@ Kohana::modules(array(
     'cache'         => MODPATH.'cache',     //cache module
 	));
 
-/**
- * Set the routes. Each route must have a minimum of a name, a URI and a set of
- * defaults for the URI.
- */
-
-/**
- * lock/unlock buildings, vehicles, rooms
- */
-Route::set('lock', 'lock(/<lock_nr>)', array('lock_nr' => '[0-9a-f]{4,12}'))
-	->defaults(array(
-        'directory' => 'user',
-		'controller' => 'lock',
-		'action'     => 'lock'
-	));
-Route::set('unlock', 'unlock(/<lock_nr>)', array('lock_nr' => '[0-9a-f]{4,12}'))
-	->defaults(array(
-        'directory' => 'user',
-		'controller' => 'lock',
-		'action'     => 'lock'
-	));
-Route::set('lock_default', 'lock(/<action>)')
-	->defaults(array(
-        'directory' => 'user',
-		'controller' => 'lock',
-		'action'     => 'index'
-	));
-
-Route::set('activate', 'activate')
-	->defaults(array(
-		'controller' => 'login',
-		'action'     => 'activate'
-	));
-
-//default admin routes
-Route::set('admin_expanded', 'admin(/<controller>(/<action>(/<id>(/<param>))))')
-	->defaults(array(
-        'directory'  => 'admin',
-		'controller' => 'menu',
-		'action'     => 'index'
-	));
-Route::set('admin', 'admin(/<controller>(/<action>(/<id>)))', array('id'=>'.*'))
-	->defaults(array(
-        'directory'  => 'admin',
-		'controller' => 'menu',
-		'action'     => 'index'
-	));
-
-Route::set('events', 'events/p(/<page>)', array('page'=>'.*'))
-	->defaults(array(
-		'controller' => 'events',
-		'action'     => 'index'
-	));
-
-Route::set('user_build', 'user/build(/<menu_id>)', array('menu_id'=>'.*'))
-	->defaults(array(
-        'directory'  => 'user',
-		'controller' => 'build',
-		'action'     => 'index'
-	));
-
-//default user routes
-Route::set('user_default', 'user(/<controller>(/<action>(/<id>)))', array('id'=>'.*'))
-	->defaults(array(
-        'directory'  => 'user',
-		'controller' => 'menu',
-		'action'     => 'index'
-	));
-//default route
-Route::set('default', '(<controller>(/<action>(/<id>)))', array('id'=>'.*'))
-	->defaults(array(
-		'controller' => 'welcome',
-		'action'     => 'index',
-	)); 
+// Load the routes
+require APPPATH . 'routes' . EXT;
---
+++ application/routes.php
@@ -0,0 +1,77 @@
+<?php
+
+/**
+ * Set the routes. Each route must have a minimum of a name, a URI and a set of
+ * defaults for the URI.
+ */
+
+/**
+ * lock/unlock buildings, vehicles, rooms
+ */
+Route::set('lock', 'lock(/<lock_nr>)', array('lock_nr' => '[0-9a-f]{4,12}'))
+	->defaults(array(
+        'directory' => 'user',
+		'controller' => 'lock',
+		'action'     => 'lock'
+	));
+Route::set('unlock', 'unlock(/<lock_nr>)', array('lock_nr' => '[0-9a-f]{4,12}'))
+	->defaults(array(
+        'directory' => 'user',
+		'controller' => 'lock',
+		'action'     => 'lock'
+	));
+Route::set('lock_default', 'lock(/<action>)')
+	->defaults(array(
+        'directory' => 'user',
+		'controller' => 'lock',
+		'action'     => 'index'
+	));
+
+Route::set('activate', 'activate')
+	->defaults(array(
+		'controller' => 'login',
+		'action'     => 'activate'
+	));
+
+//default admin routes
+Route::set('admin_expanded', 'admin(/<controller>(/<action>(/<id>(/<param>))))')
+	->defaults(array(
+        'directory'  => 'admin',
+		'controller' => 'menu',
+		'action'     => 'index'
+	));
+Route::set('admin', 'admin(/<controller>(/<action>(/<id>)))', array('id'=>'.*'))
+	->defaults(array(
+        'directory'  => 'admin',
+		'controller' => 'menu',
+		'action'     => 'index'
+	));
+
+Route::set('events', 'events/p(/<page>)', array('page'=>'.*'))
+	->defaults(array(
+		'controller' => 'events',
+		'action'     => 'index'
+	));
+
+Route::set('user_build', 'user/build(/<menu_id>)', array('menu_id'=>'.*'))
+	->defaults(array(
+        'directory'  => 'user',
+		'controller' => 'build',
+		'action'     => 'index'
+	));
+
+//default user routes
+Route::set('user_default', 'user(/<controller>(/<action>(/<id>)))', array('id'=>'.*'))
+	->defaults(array(
+        'directory'  => 'user',
+		'controller' => 'menu',
+		'action'     => 'index'
+	));
+//default route
+Route::set('default', '(<controller>(/<action>(/<id>)))', array('id'=>'.*'))
+	->defaults(array(
+		'controller' => 'welcome',
+		'action'     => 'index',
+	)); 
+
+?>
